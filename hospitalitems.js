0f245820176bbb2e8b4cb2cf418db2a848231b00
Mary-09cloud <maryadeniyi272@gmail.com>
2026-07-23 14:46:08 -0700

    hospitalitems.js

---
+++ hospitalitems.js
@@ -0,0 +1,20 @@
+const hospitalitems=[
+{name:"Ibuprofen",qty:50}
+{name:"Mentilated spirit",qty:30}
+{name:"Glove",qty:50}
+{name:"Bandage",qty:150}
+{name:"Facemask",qty:500}
+{name:"Thermometer",qty:30}
+{name:"Syringe",qty:130}
+{name:"Sanitizer",qty:80}
+{name:"Cough syrup",qty:70}
+{name:"Vitamin C",qty:200}
+{name:"Amoxicilline",qty:100}
+{name:"ORS Sachets",qty:150}
+{name:"Amoxiclav",qty:150}
+{name:"Athermeter",qty:800}
+{name:"Softgel",qty:150}
+{name:"Vitamin B complex",qty:200}
+{name:"Argumentine",qty:130}
+{name:"Neurogesic oinment",qty:300};
+module.exports=hospitalitems;
