b5b7b77ddb96670d5bd0b037982b1339ca721889
Mary-09cloud <maryadeniyi272@gmail.com>
2026-07-23 15:37:53 -0700

    hospitalitems.js

--- hospitalitems.js
+++ hospitalitems.js
@@ -1,20 +1,21 @@
 const hospitalitems=[
-{name:"Ibuprofen",qty:50}
-{name:"Mentilated spirit",qty:30}
-{name:"Glove",qty:50}
-{name:"Bandage",qty:150}
-{name:"Facemask",qty:500}
-{name:"Thermometer",qty:30}
-{name:"Syringe",qty:130}
-{name:"Sanitizer",qty:80}
-{name:"Cough syrup",qty:70}
-{name:"Vitamin C",qty:200}
-{name:"Amoxicilline",qty:100}
-{name:"ORS Sachets",qty:150}
-{name:"Amoxiclav",qty:150}
-{name:"Athermeter",qty:800}
-{name:"Softgel",qty:150}
-{name:"Vitamin B complex",qty:200}
-{name:"Argumentine",qty:130}
-{name:"Neurogesic oinment",qty:300};
+{id: 1,name:"Ibuprofen",qty:50},
+{id: 2,name:"Methylated spirit",qty:30},
+{id: 3,name:"Glove",qty:50},
+{id: 4,name:"Bandage",qty:150},
+{id: 5,name:"Facemask",qty:500},
+{id: 6,name:"Thermometer",qty:30},
+{id: 7,name:"Syringe",qty:130},
+{id: 8,name:"Sanitizer",qty:80},
+{id: 9,name:"Cough syrup",qty:70},
+{id: 10,name:"Vitamin C",qty:200},
+{id: 11,name:"Amoxicilline",qty:100},
+{id: 12,name:"ORS Sachets",qty:150},
+{id: 13,name:"Amoxiclav",qty:150},
+{id: 14,name:"Athermeter",qty:800},
+{id: 15,name:"Softgel",qty:150},
+{id: 16,name:"Vitamin B complex",qty:200},
+{id: 17,name:"Agumentine",qty:130},
+{id: 18,name:"Neurogesic ointment",qty:300}];
+
 module.exports=hospitalitems;
