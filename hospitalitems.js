28ba4c2589e8a1fbdb742fb25c28dd8711da18fe
Omollo-1 <geraldomollo942@gmail.com>
2026-07-23 23:27:08 +0300

    adding missing commas in hospitalitems list and modifying package.json from index.js to server.js

--- hospitalitems.js
+++ hospitalitems.js
@@ -1,11 +1,11 @@
 const hospitalitems=[
-{name:'Paracetamol',qty:100}
-{name:'Glove',qty:50}
-{name:'Syringe',qty:300}
-{name:'Bandage',qty:150}
-{name:'Facemask',qty:500}
-{name:'Ibuprofen',qty:90}
-{name:'Thermometer',qty:25}
-{name:'Sanitizer',qty:60}
-{name:'Cotton Wool',qty:100}
+{name:'Paracetamol',qty:100},
+{name:'Glove',qty:50},
+{name:'Syringe',qty:300},
+{name:'Bandage',qty:150},
+{name:'Facemask',qty:500},
+{name:'Ibuprofen',qty:90},
+{name:'Thermometer',qty:25},
+{name:'Sanitizer',qty:60},
+{name:'Cotton Wool',qty:100}]
 module.exports=hospitalitems;
