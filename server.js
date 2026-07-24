bd4a8e060401441325935a7c8f98bedcfa92a4f9
lamideabyyb <olamidehabeeb314@gmai.com>
2026-07-24 13:25:37 +0100

    Refixed edit for task 6

--- server.js
+++ server.js
@@ -15,6 +15,43 @@ app.get("/inventory", (req, res)=>{
     res.status(200).json(hospitalitems);
 });
 
+// / GET /items - View all hospital items
+app.get("/items", (req, res) => {
+    res.json(hospitalitems);
+});
+
+//Search for a hospital item by ID
+app.get("/items/:id", (req, res) => {
+    const id = Number(req.params.id);
+
+    const item = hospitalitems.find(item => item.id === id);
+
+    if (!item) {
+        return res.status(404).json({
+            error: "Hospital item not found"
+        });
+    }
+
+    res.status(200).json(item);
+});
+
+app.put("/items/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const { name, qty } = req.body;
+    const hospitalItemIndex = hospitalitems.findIndex(item => item.id === id);
+    if (!item){
+        return res.status(404).json({
+            success: false, 
+            message: "Hospital item not found"  
+        });
+        if (name) item.name = name;
+        if (qty) item.qty = qty;
+        res.status(200).json({
+            success: true,
+            message: "Hospital item updated successfully",
+            item
+        });
+    };
 
 /*
 TODO: Product Inventory Routes
